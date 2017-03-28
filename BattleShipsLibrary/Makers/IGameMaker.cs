8209f30a6f5f66ec05df4ccf5afad9341f573254
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-03-28 13:19:12 +0200

    The beginning

---
+++ BattleShipsLibrary/Makers/IGameMaker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipsLibrary.Makers
+{
+    public interface IGameMaker
+    {
+        void CreateBattleArea();
+    }
+}
