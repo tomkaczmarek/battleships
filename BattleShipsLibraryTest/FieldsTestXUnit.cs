8120538524978418d132e00dae7a62e0ebe26fe4
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-04-12 22:03:58 +0200

    some test in xunit

---
+++ BattleShipsLibraryTest/FieldsTestXUnit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace BattleShipsLibraryTest
+{    
+    public class FieldsTestXUnit
+    {
+        private readonly ITestOutputHelper output;
+
+        public FieldsTestXUnit(ITestOutputHelper _out)
+        {
+            output = _out;
+        }
+
+        [Fact]
+        public void XUNitShouldMakeConcreteFieldSymbol()
+        {
+            int i = 5;
+            int j = 5;
+
+            output.WriteLine("{0}", i);
+
+            Assert.Equal(i, j);
+        }
+
+    }
+}
